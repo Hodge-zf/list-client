a397d38c676bb95e2edf290d3d496bf097b4859b
Hodge-zf <xx.skcs@gmail.com>
2018-12-09 19:41:17 -0600

    Creates list of players from input

--- CountingGame.java
+++ CountingGame.java
@@ -4,11 +4,11 @@ import java.util.*;
 /**
  * CountingGame is a program that will simulate a children's counting game used to select
  * someone.
- * 
+ *
  * @author Charles Hoot
  * @version 4.0
  */
-    
+
 public class CountingGame
 {
 
@@ -16,26 +16,32 @@ public class CountingGame
     {
         ListInterface<Integer> players = null;
         ListInterface<String> rhyme = null;
-        
+
         int max;
         int position = 1;       // always start with the first player
-        
+
         System.out.println("Please enter the number of players.");
         max = getInt("   It should be an integer value greater than or equal to 2.");
         System.out.println("Constructing list of players");
-        
+
         // ADD CODE HERE TO CREATE THE LIST OF PLAYERS
-        
+
+        players = new AList<Integer>();
+
+        for(int i = 1; i <= max; i++){
+          players.add(i);
+        }
+
         System.out.println("The players list is " + players);
-        
+
         rhyme = getRhyme();
 
         // ADD CODE HERE TO PLAY THE GAME
-        
+
         System.out.println("The winner is " + players.getEntry(1));
     }
-    
-    
+
+
     /**
      * Do the rhyme with the players in the list and remove the selected
      * player.
@@ -43,7 +49,7 @@ public class CountingGame
      * @param  players   A list holding the players.
      * @param  rhyme   A list holding the words of the rhyme.
      * @param  startAt A position to start the rhyme at.
-     * 
+     *
      * @return The position of the player eliminated.
      */
    public static int doRhyme(ListInterface<Integer> players, ListInterface<String> rhyme, int startAt)
@@ -52,13 +58,13 @@ public class CountingGame
         return -1;
 
      }
-    
-    
-    
+
+
+
     /**
      * Get an integer value.
      *
-     * @return     An integer. 
+     * @return     An integer.
      */
     private static int getInt(String rangePrompt)
     {
@@ -69,14 +75,14 @@ public class CountingGame
             input = new Scanner(System.in);
             System.out.println(rangePrompt);
             result = input.nextInt();
-            
+
         }
         catch(NumberFormatException e)
         {
             System.out.println("Could not convert input to an integer");
             System.out.println(e.getMessage());
             System.out.println("Will use 10 as the default value");
-        }        
+        }
         catch(Exception e)
         {
             System.out.println("There was an error with System.in");
@@ -84,9 +90,9 @@ public class CountingGame
             System.out.println("Will use 10 as the default value");
         }
         return result;
-                                    
+
     }
-    
+
     /**
      * getRhyme - Get the rhyme.
      *
@@ -97,20 +103,20 @@ public class CountingGame
         Scanner input;
         String inString = "";
         ListInterface<String> rhyme = new AList<String>();
-        
+
         try
         {
             input = new Scanner( System.in );
-            
+
             System.out.println("Please enter a rhyme");
             inString = input.nextLine().trim();
-            
+
             Scanner rhymeWords = new Scanner(inString);
             while(rhymeWords.hasNext())
             {
                 rhyme.add(rhymeWords.next());
             }
-            
+
         }
         catch(Exception e)
         {
@@ -122,9 +128,9 @@ public class CountingGame
         // Make sure there is at least one word in the rhyme
         if(rhyme.getLength() < 1)
             rhyme.add("Default");
-            
+
         return (ListInterface<String>)rhyme;
-                                    
+
     }
-    
+
 }
